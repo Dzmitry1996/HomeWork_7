aae695f55f7fea25029c20f81f2b6b5c11fb4f4d
Dzmitry Glebik <banan1314@gmail.com>
2021-12-01 22:51:00 +0300

    First commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let Calc = function() {
+/*let Calc = function() {
     this.onOff = function() {
        this.turn = confirm('Включить калькулятор?');
        if (this.turn) {
@@ -53,4 +53,37 @@ let Calc = function() {
  
  let calc = new Calc();
  
- calc.onOff();
+ calc.onOff();*/
+
+
+ let a = '';          //first number
+ let b = '';          //secont number
+ let sign = '';       // знак операции
+ let finish = false;
+
+ const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.',];
+ const action = ['-', '+', 'X', '/',];
+
+ //экран
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+   a = ''; //first number and result
+   b = ''; //second number
+   sign = ''; //знак
+   finish = false
+   out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+   // нажата не кнопка
+   if(!event.target.classList.contains('btn')) return;
+   //нажата кноака ас
+   if(!event.target.classList.contains('ac')) return;
+
+   out.textContent = '';
+   //получаю нажатую кнопку
+   const key =event.target.textContent;
+};
